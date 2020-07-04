d34fef34e2bd8b854915fa900f645aef24189bac
TrueNoob141 <truenoob141@gmail.com>
2020-07-04 13:57:29 +0300

    Installers: created basic installers

---
+++ Assets/Scripts/Installers/GameSettingsInstaller.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace Mine
+{
+    [CreateAssetMenu(fileName = "GameSettings", menuName = "Installers/GameSettingsInstaller")]
+    public class GameSettingsInstaller : ScriptableObjectInstaller<GameSettingsInstaller>
+    {
+        public ProjectInstaller.Settings settings;
+
+        public override void InstallBindings()
+        {
+            Container.BindInstance(this.settings);
+        }
+    }
+}
---
+++ Assets/Scripts/Installers/ProjectInstaller.cs
@@ -0,0 +1,28 @@
+﻿using Mine.Game;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+namespace Mine
+{
+    public class ProjectInstaller : MonoInstaller
+    {
+        [System.Serializable]
+        public class Settings
+        {
+            public GameObject statPrefab;
+            [Space(10)]
+            public int healthId;
+            public int armorId;
+            public int damageId;
+            public int lifestealId;
+        }
+
+        [Inject] private Settings settings;
+
+        public override void InstallBindings()
+        {
+            Container.BindInterfacesAndSelfTo<PoolCleanupChecker>().AsSingle();
+        }
+    }
+}
