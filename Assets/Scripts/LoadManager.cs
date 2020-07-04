b71094fcaf33bbfc45cba81143c30ea74aaed47a
TrueNoob141 <truenoob141@gmail.com>
2020-07-04 13:58:45 +0300

    LoadManager: created

---
+++ Assets/Scripts/LoadManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using Zenject;
+
+namespace Mine
+{
+    public class OnGameLoaded { }
+
+    public class LoadManager : IInitializable
+    {
+        public Data GameData { get; private set; }
+
+        [Inject] private EventManager eventManager;
+
+        public void Initialize()
+        {
+            var request = Resources.LoadAsync<TextAsset>("data");
+            request.completed += (op) =>
+            {
+                var config = (TextAsset)request.asset;
+                Assert.IsNotNull(config, "Config not found");
+
+                this.GameData = JsonUtility.FromJson<Data>(config.text);
+
+                eventManager.Trigger<OnGameLoaded>();
+            };
+        }
+    }
+}
