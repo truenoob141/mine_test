391a234e2b436e6b6b7a2bf3f68426a61368d998
TrueNoob141 <truenoob141@gmail.com>
2020-07-04 14:01:52 +0300

    StartGameButton: created

---
+++ Assets/Scripts/StartGameButton.cs
@@ -0,0 +1,41 @@
+﻿using Mine.Game;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+namespace Mine
+{
+    [RequireComponent(typeof(Button))]
+    public class StartGameButton : MonoBehaviour
+    {
+        [SerializeField]
+        private bool withBuffs;
+
+        [Inject] private GameManager gameManager;
+
+        private Button button;
+
+        private void Awake()
+        {
+            this.button = GetComponent<Button>();
+        }
+
+        private void OnEnable()
+        {
+            this.button.onClick.AddListener(OnClick);
+        }
+
+        private void OnDisable()
+        {
+            this.button.onClick.RemoveListener(OnClick);
+        }
+
+        private void OnClick()
+        {
+            if (gameManager.IsValidGame)
+                gameManager.EndGame();
+
+            gameManager.StartGame(this.withBuffs);
+        }
+    }
+}
